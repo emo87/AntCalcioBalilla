196d88957ca5298ab0e7244884d1b34797873fa3
Fabio <lentini.consulting@gmail.com>
2016-02-25 14:18:11 +0100

    test

--- app/src/main/java/com/it/antares/antarescalciobalilla/MainActivity.java
+++ app/src/main/java/com/it/antares/antarescalciobalilla/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "pippo", Toast.LENGTH_SHORT).show();
     }
 
     @Override
