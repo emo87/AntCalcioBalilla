3f83ca6e9e5a08fe939167f19b2658e655d178f4
emo87 <emilio.marino@hotmail.it>
2016-02-25 11:42:45 +0100

    2 - test

---
+++ app/src/main/java/com/it/antares/antarescalciobalilla/MainActivity.java
@@ -0,0 +1,207 @@
+package com.it.antares.antarescalciobalilla;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.it.antares.antarescalciobalilla.adapter.DrawerAdapter;
+import com.it.antares.antarescalciobalilla.model.DrawerItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ListView mDrawerList;
+    private List<DrawerItem> mDrawerItems;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+
+    private Handler mHandler;
+
+    private boolean mShouldFinish = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //	ImageLoader imageLoader = ImageLoader.getInstance();
+        //	if (!imageLoader.isInited()) {
+        //		imageLoader.init(ImageLoaderConfiguration.createDefault(this));
+//		}
+
+     //   Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+      //  setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(mTitle);
+                supportInvalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(mDrawerTitle);
+                supportInvalidateOptionsMenu();
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerList = (ListView) findViewById(R.id.list_view);
+
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+                GravityCompat.START);
+        prepareNavigationDrawerItems();
+        mDrawerList.setAdapter(new DrawerAdapter(this, mDrawerItems, true));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mHandler = new Handler();
+
+        if (savedInstanceState == null) {
+            int position = 0;
+            selectItem(position, mDrawerItems.get(position).getTag());
+            mDrawerLayout.openDrawer(mDrawerList);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!mShouldFinish && !mDrawerLayout.isDrawerOpen(mDrawerList)) {
+            Toast.makeText(getApplicationContext(), R.string.confirm_exit,
+                    Toast.LENGTH_SHORT).show();
+            mShouldFinish = true;
+            mDrawerLayout.openDrawer(mDrawerList);
+        } else if (!mShouldFinish && mDrawerLayout.isDrawerOpen(mDrawerList)) {
+            mDrawerLayout.closeDrawer(mDrawerList);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void prepareNavigationDrawerItems() {
+        mDrawerItems = new ArrayList<>();
+     /*   mDrawerItems.add(new DrawerItem(R.string.drawer_icon_list_views,
+                R.string.drawer_title_list_views,
+                DrawerItem.DRAWER_ITEM_TAG_LIST_VIEWS));
+*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements
+            ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                                long id) {
+            selectItem(position, mDrawerItems.get(position).getTag());
+        }
+    }
+
+    private void selectItem(int position, int drawerTag) {
+        Fragment fragment = getFragmentByDrawerTag(drawerTag);
+        commitFragment(fragment);
+
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mDrawerItems.get(position).getTitle());
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    private Fragment getFragmentByDrawerTag(int drawerTag) {
+        Fragment fragment;
+     /*   if (drawerTag == DrawerItem.DRAWER_ITEM_TAG_SPLASH_SCREENS) {
+            fragment = SplashScreensFragment.newInstance();
+            mShouldFinish = false;
+            return fragment;
+       */
+        return null;
+    }
+
+
+    public void commitFragment(Fragment fragment) {
+        // Using Handler class to avoid lagging while
+        // committing fragment in same time as closing
+        // navigation drawer
+        mHandler.post(new CommitFragmentRunnable(fragment));
+    }
+
+    @Override
+    public void setTitle(int titleId) {
+        setTitle(getString(titleId));
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+    private class CommitFragmentRunnable implements Runnable {
+
+
+        private Fragment fragment;
+
+        public CommitFragmentRunnable(Fragment fragment) {
+            this.fragment = fragment;
+        }
+
+        @Override
+        public void run() {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment).commit();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/it/antares/antarescalciobalilla/model/DrawerItem.java
@@ -0,0 +1,41 @@
+package com.it.antares.antarescalciobalilla.model;
+
+public class DrawerItem {
+	/* Commented tags are expected in future updates.
+	 */
+
+	
+	public DrawerItem(int icon, int title, int tag) {
+		this.icon = icon;
+		this.title = title;
+		this.tag = tag;
+	}
+
+	private int icon;
+	private int title;
+	private int tag;
+
+	public int getIcon() {
+		return icon;
+	}
+
+	public void setIcon(int icon) {
+		this.icon = icon;
+	}
+
+	public int getTitle() {
+		return title;
+	}
+
+	public void setTitle(int title) {
+		this.title = title;
+	}
+
+	public int getTag() {
+		return tag;
+	}
+
+	public void setTag(int tag) {
+		this.tag = tag;
+	}
+}
