bd2ffd773c53eb0a9be622d53c690c660b563f8f
emo87 <emilio.marino@hotmail.it>
2016-02-25 14:30:50 +0100

    non devono andare su github

--- app/src/main/java/com/it/antares/antarescalciobalilla/MainActivity.java
+++ app/src/main/java/com/it/antares/antarescalciobalilla/MainActivity.java
@@ -6,13 +6,7 @@ import java.util.List;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
